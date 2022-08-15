ce7cca194aafd36ccfd8c90ecf567935ec4ae24d
farihaorna23 <farihaorna@gmail.com>
2022-08-15 00:23:07 -0400

    finished exercise3

--- app.js
+++ app.js
@@ -2,3 +2,9 @@
 const firstName = "Fariha";
 let lastName = "Hossain";
 var age = 24;
+console.log(firstName, lastName, age);
+//Exercise 3
+let language = "Javascript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+console.log(language, createdYear, isCaseSensitive);
